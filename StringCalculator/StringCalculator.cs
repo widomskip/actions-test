27b499b7789990655ff741f4382442f8d87b07a1
molom <venmolom@gmail.com>
2021-03-23 20:17:27 +0100

    first

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeKata
+{
+    public class StringCalculator
+    {
+        private static readonly string[] DELIMITERS = new string[] { ",", "\n" }; 
+
+        public int Add(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+            {
+                return 0;
+            }
+
+            var match = Regex.Match(number, @"^//((\[.+\])+|.{1})");
+            var foundDelimiters = match.Value.TrimStart('/').Split("][", StringSplitOptions.RemoveEmptyEntries).Select(m => m.Trim('[', ']'));
+
+            if (match.Success)
+            {
+                number = number.Substring(match.Index + match.Length);
+            }
+
+            var delimiters = DELIMITERS.Concat(foundDelimiters);
+
+            var values = number.Split(delimiters.ToArray(), StringSplitOptions.None);
+
+            var sum = 0;
+            foreach (var value in values)
+            {
+                var num = int.Parse(value);
+                if(num < 0)
+                {
+                    throw new ArgumentException();
+                }
+                if(num > 1000)
+                {
+                    continue;
+                }
+                sum += num;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Tests
+{
+    [TestClass()]
+    public class StringCalculatorTests
+    {
+        [TestMethod()]
+        public void ShouldReturnZeroForEmptyString()
+        {
+            var stringCalculator = new StringCalculator();
+
+            var result = stringCalculator.Add(string.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnValueForSingleNumber()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "52";
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(int.Parse(value), result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnSumForTwoNumbersCommaDelimited()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "5,4";
+            var expected = 9;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnSumForTwoNumbersNewlineDelimited()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "5\n4";
+            var expected = 9;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnSumForThreeNumbersDelimitedEitherWay()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "5,4\n1";
+            var expected = 10;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowExceptionForNegativeNumbers()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "-5";
+
+            stringCalculator.Add(value);
+        }
+
+        [TestMethod()]
+        public void ShouldIgnoreNumbersGreaterThan1000()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "5,4000\n1";
+            var expected = 6;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldAcceptDefiningSingleCharDelimiters()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "//#5#4\n1";
+            var expected = 10;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptDefiningMultiCharDelimiters()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "//[###]5###4\n1";
+            var expected = 10;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptDefiningManySingleAndMultiCharDelimiters()
+        {
+            var stringCalculator = new StringCalculator();
+            var value = "//[###][%%%]5###4%%%1";
+            var expected = 10;
+
+            var result = stringCalculator.Add(value);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
